eedec8013f6e047cd3a9f95ea81287590413fc8f
David <peelp50z@protonmail.com>
2023-04-03 10:46:15 -0400

    there was a syntax error in advnaced driver, changed column names

--- src/AdvancedDriver.java
+++ src/AdvancedDriver.java
@@ -6,7 +6,7 @@ public class AdvancedDriver extends Car {
     @Override
     public Move nextMove(Cell[][] grid) {
         Road r = (Road) grid[getY()][getX()];
-        if (r.canMoveForward(getDirection()) && r.) {
+        if (r.canMoveForward(getDirection())) {
             addMove(Move.FORWARD);
             return Move.FORWARD;
         } else if (r.canTurnLeft(getDirection())) {
